600bfd3c277fd3d995b7eb64d4af92711052ebd4
ruozziriccardo4 <ruozziriccardo@4gmail.com>
2026-02-24 10:04:37 +0100

    db, table e row delete

--- main.c
+++ main.c
@@ -49,6 +49,47 @@ void printTable(Table table){
     }
 }
 
+void dropDB(Database *db){
+    for(int i=0; i<db->table_number; i++){
+        dropTable(&db->tables[i]);
+    }
+    free(db->tables);
+    db->table_number=0; 
+    db->tables = NULL; 
+    printf("Database eliminato\n");
+}
+
+void dropTable(Table* table){
+    for(int i=0; i<table->page_number; i++){
+        free(table->pages[i].rows);
+    }
+    free(table->pages);
+    table->page_number=0;
+    table->pages = NULL;
+    printf("Tabella eliminata\n");
+}
+
+void deleteRow(Table *table, int id){
+    int num_page = id * sizeof(Row) / PAGE_SIZE; 
+    if(num_page >= table->page_number){
+        printf("ID non valido\n");
+        return;
+    }
+    Page* page = &table->pages[num_page];
+    for(int i=0; i<page->row_count; i++){
+        for(int j=0; j < page->row_count; j++){
+            if(page->rows[j].id == id){
+                for(int k=j; k<page->row_count - 1; k++){
+                    page->rows[k] = page->rows[k+1];
+                }
+                page->row_count--;
+                printf("Riga eliminata\n");
+                return;
+            }
+        }
+    }
+}
+
 int main(){
     Database db = createDatabase("my_database");
     db = createTable(db, "users");
